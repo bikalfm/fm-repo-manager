0e383f544e517c75630c514ffe564b864b97f36b
Bikal Lamichhane <bikal@finalmoment.xyz>
2025-05-02 20:12:00 -0500

    update base api url

--- src/pages/RepositoryDetail/RepositoryDetail.tsx
+++ src/pages/RepositoryDetail/RepositoryDetail.tsx
@@ -347,7 +347,7 @@ const RepositoryDetail: React.FC = () => {
       <div className="md:flex md:items-center md:justify-between mb-6">
         <div className="flex-1 min-w-0">
           <h2 className="text-2xl font-bold leading-7 text-white sm:text-3xl sm:truncate mb-2">
-            Repository: {repositoryName}
+            {repositoryName}
           </h2>
           <div className="mt-1">
             {renderBreadcrumbs()}
